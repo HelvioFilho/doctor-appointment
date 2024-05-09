b4cab076a10b78725f8f354dd69afe3b01d53456
Hélvio Filho <helviosvf@gmail.com>
2024-05-09 17:04:33 -0300

    feat: gets the data via graphQL from the backend

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,40 +1,57 @@
+import { useQuery, gql } from "@apollo/client";
 import { Dimensions, FlatList, Image, Text, View } from "react-native";
+import { Loading } from "./Loading";
+
+const GET_SLIDERS = gql`
+  query GetSliders {
+    getSliders {
+      id
+      name
+      imageUrl
+    }
+  }
+`;
+
+type SliderList = {
+  id: number;
+  name: string;
+  imageUrl: string;
+};
 
 export function Slider() {
-  const silderList = [
-    {
-      id: 1,
-      name: "slider 1",
-      imageUrl:
-        "https://photomedia.in/wp-content/uploads/2023/04/yellow-red-abstract-background-2048x1368.jpg",
-    },
-    {
-      id: 2,
-      name: "slider 2",
-      imageUrl:
-        "https://photomedia.in/wp-content/uploads/2023/04/red-blue-abstruct-background-2048x1365.jpg",
-    },
-    {
-      id: 3,
-      name: "slider 3",
-      imageUrl:
-        "https://photomedia.in/wp-content/uploads/2023/04/yellow-abstruct-background-2048x1365.jpg",
-    },
-  ];
+  // const [getSliders, setSliders] = useState<SliderList[]>([]);
+  const { data, loading, error } = useQuery<{ getSliders: SliderList[] }>(
+    GET_SLIDERS
+  );
+
+  if (loading)
+    return (
+      <View className="h-44 justify-center">
+        <Loading />
+      </View>
+    );
+  if (error) {
+    console.log(error);
+    return <Text>Error: {error.message} </Text>;
+  }
+
   return (
     <View className="mt-3">
       <FlatList
-        data={silderList}
+        data={data?.getSliders}
         horizontal={true}
         showsHorizontalScrollIndicator={false}
         keyExtractor={(item) => item.id.toString()}
-        renderItem={({ item, index }) => (
-          <Image
-            source={{ uri: item.imageUrl }}
-            className="h-44 rounded-lg m-1"
-            style={{ width: Dimensions.get("screen").width * 0.9 }}
-          />
-        )}
+        renderItem={({ item }) => {
+          return (
+            <Image
+              source={{ uri: item.imageUrl }}
+              className="h-44 rounded-lg m-1"
+              alt=""
+              style={{ width: Dimensions.get("screen").width * 0.9 }}
+            />
+          );
+        }}
       />
     </View>
   );
