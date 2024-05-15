687469b45e68b4d3210a764cd18c2078605dcdda
Hélvio Filho <helviosvf@gmail.com>
2024-05-15 19:23:24 -0300

    feat: add the link to the map app on your cell phone

--- src/components/HospitalCardItem.tsx
+++ src/components/HospitalCardItem.tsx
@@ -1,5 +1,6 @@
-import { Image, Text, View } from "react-native";
+import { Image, Platform, Pressable, Text, View } from "react-native";
 import { AntDesign, Ionicons } from "@expo/vector-icons";
+import * as Linking from "expo-linking";
 
 import { GetHospitalsByCategoryData } from "./HospitalList";
 
@@ -20,6 +21,21 @@ export function HospitalCardItem({ data }: HospitalCardItemProps) {
     address = address.slice(0, 45) + "...";
   }
 
+  const openMap = (address: string) => {
+    const url = Platform.select({
+      ios: `http://maps.apple.com/?daddr=${address}&dirflg=d&t=h`,
+      android: `geo:0,0?q=${address}`,
+      web: `https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(
+        address
+      )}`,
+    });
+
+    url &&
+      Linking.openURL(url).catch((err) =>
+        console.error("Couldn't load page", err)
+      );
+  };
+
   return (
     <View className="rounded-xl mb-5 border border-gray-400">
       <Image
@@ -30,12 +46,14 @@ export function HospitalCardItem({ data }: HospitalCardItemProps) {
         <Text className="font-semibold text-lg">{data.name}</Text>
         <Text className="font-regular text-gray-600">{categoriesString}</Text>
         <View className="border-b border-gray-300 m-1 mb-3" />
-        <View className="flex-row items-center gap-1.5">
-          <Ionicons name="location" size={20} color={colors.primary} />
-          <Text className="font-regular text-base text-gray-600">
-            {address}
-          </Text>
-        </View>
+        <Pressable onPress={() => openMap(data.address)}>
+          <View className="flex-row items-center gap-1.5">
+            <Ionicons name="location" size={20} color={colors.primary} />
+            <Text className="font-regular text-base text-gray-600">
+              {address}
+            </Text>
+          </View>
+        </Pressable>
         {data.duration && data.distance && (
           <View className="flex-row items-center gap-1.5 pl-1 mt-2">
             <AntDesign name="clockcircle" size={15} color={colors.primary} />
