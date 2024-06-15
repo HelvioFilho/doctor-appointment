1d7987ad64ca1dc2222fc12f1086ea8b8e7847b3
Hélvio Filho <helviosvf@gmail.com>
2024-06-15 13:34:51 -0300

    feat: remove header from the screen

--- src/app/hospital/_layout.tsx
+++ src/app/hospital/_layout.tsx
@@ -2,7 +2,7 @@ import { Stack } from "expo-router";
 
 export default function HospitalLayout() {
   return (
-    <Stack>
+    <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="hospitalDoctorsList" />
       <Stack.Screen name="hospitalDetails" />
     </Stack>
